f62a5da658eb64a418b626abdc245df448a8fe38
Jayant Surana <jayantplb@gmail.com>
2019-10-16 00:32:14 +0530

    Add Two Numbers | Problem Code: FLOW001
    C++
    CodeChef

---
+++ CodeChef/Practice/beginner/Add Two Numbers.cpp
@@ -0,0 +1,18 @@
+// This solution is provided by CodeChef.
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while (T--) {
+		int a, b;
+		cin >> a >> b;
+
+		int ans = a + b;
+		cout << ans << endl;
+	}
+
+	return 0;
+}

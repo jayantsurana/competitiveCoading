f692a3fcf0ee0c0ade79cfd5a348dbb9c6a67e3d
Jayant Surana <jayantplb@gmail.com>
2019-10-16 00:27:18 +0530

    Enormous Input Test | Problem Code: INTEST
    C++
    CodeChef

---
+++ CodeChef/Practice/beginner/Enormous Input Test.cpp
@@ -0,0 +1,28 @@
+// This solution is provided by CodeChef.
+
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, k;
+	cin >> n >> k;
+
+	int ans = 0;
+
+	for (int i = 0; i < n; i++) {
+		int t;
+		cin >> t;
+		
+		if (t % k == 0) {
+			ans++;
+		}		
+	}
+
+	cout << ans << endl;	
+		
+	return 0;
+}
